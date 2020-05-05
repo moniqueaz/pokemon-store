8faba494e0e86e4f799d35804b7dfb346c89adf9
“Monique <moniqueaz.si@gmail.com>
2020-05-05 18:17:10 -0300

    Adicionar tema dragon

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,103 +1,41 @@
 import theme from 'styled-theming';
 
-export const backgroundColor = theme('mode', {
-  fire: '#FF0101',
-  water: '#0099E0',
-  dragon: '',
-});
-
-export const textColor = theme('mode', {
-  fire: '#fff',
-  water: '#fff',
-  dragon: '',
-});
-
-export const backgroundColorSecondary = theme('mode', {
-  fire: '#FFFC19',
-  water: '#E07A00',
-  dragon: '',
-});
-
-export const textColorSecondary = theme('mode', {
-  fire: '#272A2B',
-  water: '#272A2B',
-  dragon: '',
-});
-
-export const backgroundFooter = theme('mode', {
-  fire: '#FF0101',
-  water: '#0099E0',
-  dragon: '',
-});
-
-export const textColorFooter = theme('mode', {
-  fire: '#fff',
-  water: '#fff',
-  dragon: '',
-});
-
-// Button
-
-export const textButtonColor = theme('mode', {
-  fire: '#fff',
-  water: '#fff',
-  dragon: '',
-});
-
-export const backgroundButtonColor = theme('mode', {
-  fire: '#3C7800',
-  water: '#0BE0B0',
-  dragon: '',
-});
-
-export const textButtonColorDisabled = theme('mode', {
-  fire: '#272A2B',
-  water: '#272A2B',
-  dragon: '',
-});
-
-export const backgroundButtonColorDisabled = theme('mode', {
-  fire: '#6C7378',
-  water: '#6C7378',
-  dragon: '',
-});
-
 // Colors
 
 export const primary = theme('mode', {
   fire: '#FF0101',
   water: '#0099E0',
-  dragon: '',
+  dragon: '#E08B02',
 });
 
 export const primaryLight = theme('mode', {
   fire: '#FF4D4D',
   water: '#19B6FF',
-  dragon: '',
+  dragon: '#FFA81C',
 });
 
 export const primaryDark = theme('mode', {
   fire: '#800000',
   water: '#006594',
-  dragon: '',
+  dragon: '#945B01',
 });
 
 export const secondary = theme('mode', {
   fire: '#FFFC19',
   water: '#E07A00',
-  dragon: '',
+  dragon: '#0277E0',
 });
 
 export const secondaryLight = theme('mode', {
   fire: '#FFFC66',
   water: '#FF9819',
-  dragon: '',
+  dragon: '#1C95FF',
 });
 
 export const secondaryDark = theme('mode', {
   fire: '#807E0D',
   water: '#945100',
-  dragon: '',
+  dragon: '#014F94',
 });
 
 export const black = theme('mode', {
@@ -133,11 +71,73 @@ export const grayDark = theme('mode', {
 export const error = theme('mode', {
   fire: '#C90241',
   water: '#E01631',
-  dragon: '',
+  dragon: '#E00DBE',
 });
 
 export const success = theme('mode', {
   fire: '#3C7800',
   water: '#0BE0B0',
-  dragon: '',
+  dragon: '#10948C',
+});
+
+export const backgroundColor = theme('mode', {
+  fire: primary,
+  water: primary,
+  dragon: primary,
+});
+
+export const textColor = theme('mode', {
+  fire: white,
+  water: white,
+  dragon: white,
+});
+
+export const backgroundColorSecondary = theme('mode', {
+  fire: secondary,
+  water: secondary,
+  dragon: secondary,
+});
+
+export const textColorSecondary = theme('mode', {
+  fire: black,
+  water: black,
+  dragon: white,
+});
+
+export const backgroundFooter = theme('mode', {
+  fire: primary,
+  water: primary,
+  dragon: primary,
+});
+
+export const textColorFooter = theme('mode', {
+  fire: white,
+  water: white,
+  dragon: white,
+});
+
+// Button
+
+export const textButtonColor = theme('mode', {
+  fire: white,
+  water: white,
+  dragon: white,
+});
+
+export const backgroundButtonColor = theme('mode', {
+  fire: success,
+  water: success,
+  dragon: success,
+});
+
+export const textButtonColorDisabled = theme('mode', {
+  fire: black,
+  water: black,
+  dragon: black,
+});
+
+export const backgroundButtonColorDisabled = theme('mode', {
+  fire: gray,
+  water: gray,
+  dragon: gray,
 });
